aa5107fca898d0e5847d0f41ad81aa6cf832261c
Ryan Collingham <ryan.collingham@flare.network>
2025-06-19 18:47:39 +0100

    fix test snapshots

--- indexer/indexer_test.go
+++ indexer/indexer_test.go
@@ -110,9 +110,12 @@ func TestIndexer(t *testing.T) {
 	require.NoError(t, err)
 
 	// Set the update timestamps to zero for the snapshot as these will
-	// vary with current system  time.
+	// vary with current system  time. Also set the IDs to zero as these
+	// depend on a race condition between the different states being
+	// inserted concurrently.
 	for _, state := range states.States {
 		state.Updated = time.Time{}
+		state.ID = 0
 	}
 
 	cupaloy.SnapshotT(t, states)
